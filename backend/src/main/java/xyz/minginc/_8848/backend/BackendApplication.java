dcfd815c7250b44700fcf176e4e6daa821b8f762
13x54n <laxman.rai.07.26@gmail.com>
2026-03-12 17:32:54 -0400

    Working on frontend:angluar

---
+++ backend/src/main/java/xyz/minginc/_8848/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.minginc._8848.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/xyz/minginc/_8848/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.minginc._8848.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
